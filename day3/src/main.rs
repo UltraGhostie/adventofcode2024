ca11ada7ae93f438ed0f5079fcfaa8ca3fe8ad17
Theodor Bjorkman <theodor.bjorkman@gmail.com>
2024-12-03 11:05:13 +0100

    day3 part 2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -6,10 +6,39 @@ use regex::Regex;
 
 fn main() -> io::Result<()> {
     let _ = part1();
+    let _ = part2();
     Ok(())
 }
 
 fn part2() -> io::Result<()> {
+    let file = File::open("input.in")?;
+    //let file = File::open("test2.in")?;
+    let mut reader = BufReader::new(file);
+    let wrap = Regex::new(r"(?:mul\(([0-9]+),([0-9]+)\))|(?:do(?:n't)?\(\))").unwrap();
+
+    let mut buffer = String::new();
+    reader.read_line(&mut buffer)?;
+
+
+    let mut doornt = true;
+    let mut total = 0;
+    while buffer.len() > 1 {
+        let groupiter = wrap.captures_iter(&buffer);
+
+        for s in groupiter {
+            let dos = &s[0];
+            if dos == "do()" { doornt = true; continue }
+            if dos == "don't()" { doornt = false; continue }
+            let a = &s[1].parse::<i32>().unwrap();
+            let b = &s[2].parse::<i32>().unwrap();
+            if doornt { total += a * b };
+        }
+
+
+        buffer = String::new();
+        reader.read_line(&mut buffer)?;
+    }
+    println!("{total}");
     Ok(())
 }
 
@@ -31,7 +60,6 @@ fn part1() -> io::Result<()> {
             let a = &s[1].parse::<i32>().unwrap();
             let b = &s[2].parse::<i32>().unwrap();
             total += a * b;
-            println!("{a} {b}");
         }
 
 
